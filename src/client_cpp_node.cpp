fdcc444930c78d7cbfab533381ec017a19daae17
dn0908 <94898107+dn0908@users.noreply.github.com>
2022-01-06 12:18:41 +0900

    Update client_cpp_node.cpp

--- src/client_cpp_node.cpp
+++ src/client_cpp_node.cpp
@@ -1,17 +1,17 @@
 #include <ros/ros.h>
-#include <dyros_practice_2/ABAngle.h>
+#include <2nd_cosines_law/ABAngle.h>
 
 int main(int argc, char **argv) {
-	ros::init(argc, argv, "dyros_client");
+	ros::init(argc, argv, "ros_client");
 	if (argc != 4)
 	{
 		ROS_INFO("usage: client_cpp_node a b angle(degree)");
 		return 1;
 	}
 	ros::NodeHandle nh;
-	ros::ServiceClient srv_client = nh.serviceClient<dyros_practice_2::ABAngle>("/cosine_calc");
+	ros::ServiceClient srv_client = nh.serviceClient<2nd_cosines_law::ABAngle>("/cosine_calc");
 
-	dyros_practice_2::ABAngle srv;
+	2nd_cosines_law::ABAngle srv;
 	srv.request.a = atof(argv[1]);
 	srv.request.b = atof(argv[2]);
 	srv.request.angle = atof(argv[3]) * M_PI / 180.;
